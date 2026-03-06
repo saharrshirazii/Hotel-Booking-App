e0ec1acbebd6063200a83d2e6570093f4031ed75
saharrshirazii <saharr.shirazii@gmail.com>
2026-03-06 12:29:01 +0100

    updated all rooms

--- client/src/Pages/AllRooms.jsx
+++ client/src/Pages/AllRooms.jsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import { assets, facilityIcons, roomsDummyData } from "../assets/assets"
 import StarRating from "../Components/StarRating"
 import { useNavigate } from "react-router-dom"
 
 const AllRooms = () => {
 const navigate = useNavigate();
+const [openFilters , setOpenFilters] = useState(false);
+
     return(
         <div className="flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:px-16
         lg:px-24 xl:px-32">
@@ -48,9 +50,19 @@ const navigate = useNavigate();
                 )}
             </div>
             {/* Filters */}
-            <div>
-                
-
+            <div className="bg-white w-80 border border-gray-300 text-gray-600 max-lg:mb-8 min-lg:mt-16">
+                <div className={`flex items-center justify-between px-5 py-2.5 min-lg:border-b border-gray-300 ${openFilters && "border-b"}`}>
+                    <p className="text-base font-medium text-grag-800">FILTERS</p>
+                    <div className="text-xs cursor-pointer">
+                        <span 
+                        className="lg:hidden"
+                        onClick = {()=> setOpenFilters(!openFilters)}>
+                            {openFilters ? 'HIDE' : 'SHOW'}
+                        </span>
+                        <span className="hidden lg:block">CLEAR</span>
+                    </div>
+                    
+                </div>
             </div>
             
         </div>
@@ -59,3 +71,4 @@ const navigate = useNavigate();
 }
 
 export default AllRooms
+
