61e272ac518c60ed2da9387d8e0b89fa5e085f2f
saharrshirazii <saharr.shirazii@gmail.com>
2026-04-07 13:22:46 +0200

    finished frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,27 +7,38 @@ import AllRooms from './Pages/AllRooms'
 import { RoomDetails } from './Pages/RoomDetails'
 import MyBookings from './Pages/MyBookings'
 import { HotelReg } from './Components/HotelReg'
+import Layout from './Pages/hotelOwner/Layout'
+import AddRoom from './Pages/hotelOwner/AddRoom'
+import ListRoom from './Pages/hotelOwner/ListRoom'
+import Dashboard from './Pages/hotelOwner/dashboard'
 
-const App = ()=>{
+
+const App = () => {
 
   //we do not show the navbar in owner dashboard.
   const isOwnerPath = useLocation().pathname.includes('owner');
-  return(
+  return (
     <div>
-      {!isOwnerPath && <Navbar/>}
-      {false && <HotelReg/>}
-        <div className='min-h-[70vh]'>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/rooms' element={<AllRooms/>}/>
-            <Route path='/rooms/:id' element={<RoomDetails/>}/>
-            <Route path='/my-bookings' element={<MyBookings/>}/>
+      {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<AllRooms />} />
+          <Route path='/rooms/:id' element={<RoomDetails />} />
+          <Route path='/my-bookings' element={<MyBookings />} />
+          <Route path='/owner' element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='add-room' element={<AddRoom />} />
+            <Route path='list-room' element={<ListRoom />} />
 
-          </Routes>
-          <Footer/>
-        </div>
+          </Route>
+        </Routes>
+      </div>
+              <Footer />
 
-      
+
+
     </div>
   )
 }
