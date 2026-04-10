1c1a325ca4d952c92789b3a4b7ef7c0f182cf709
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2026-04-10 18:21:06 +0200

    Remove file extension from Navbar import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './Components/Navbar.jsx'
+import Navbar from './Components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './Pages/Home'
 import Footer from './Components/Footer'
