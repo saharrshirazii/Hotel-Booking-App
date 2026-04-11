5460d9cc70cfaff234228638428725c0842d4a3c
saharrshirazii <saharr.shirazii@gmail.com>
2026-04-11 13:58:56 +0200

    started to create backend

---
+++ Server/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import connectDB from "./configs/db.js";
+
+connectDB()
+
+const app = express();
+app.use(cors()) //Enable Cors-Origin Resource Sharing
+
+app.get ('/' , (req , res) => res.send("API is working"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT , () => console.log(`Server running on port ${PORT}`));
