eb676b7c7331a2a8d240ab09fea0662a780dcbef
saharrshirazii <saharr.shirazii@gmail.com>
2026-04-16 09:00:13 +0200

    change the clerk webhook secret

--- Server/configs/db.js
+++ Server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => { //An asynchronous function because connecting to a database takes time.
     try {
         mongoose.connection.on('connected' , ()=> console.log("Database Connected")); //An event listener. It prints "Database Connected" to your console only once the connection is successful.
-        await mongoose.connect(`${process.env.MONGODB_URL}/hotel-booking`) //This is the actual connection command. It uses the MONGODB_URL from my .env file.
+        await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`) //This is the actual connection command. It uses the MONGODB_URL from my .env file.
     }catch (error) {
         console.log(error.message);
         
