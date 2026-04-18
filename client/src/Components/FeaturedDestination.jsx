c4b3447ec16936ba6dfa4cefc4d9be51cb55e1df
saharrshirazii <saharr.shirazii@gmail.com>
2026-04-18 09:42:20 +0200

    fixed hotelcard bug

--- client/src/Components/FeaturedDestination.jsx
+++ client/src/Components/FeaturedDestination.jsx
@@ -18,6 +18,7 @@ const FeaturedDestination = ()=>{
                 {roomsDummyData.slice(0,4).map((room, index)=>(
                     <HotelCard key={room._id} room={room} index={index}/>
                 ))}
+                
             </div>
 
             <button onClick={()=>{navigate('/rooms'); scroll(0,0)}} className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>View All Destinations</button>
