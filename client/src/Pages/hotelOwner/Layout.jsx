0abd474e484c4475d159339d81ee2431162e80ec
saharrshirazii <saharr.shirazii@gmail.com>
2026-08-05 13:54:29 +0200

    updated layout.jsx file and get isOner from context file to check if the user is hotel owner

--- client/src/Pages/hotelOwner/Layout.jsx
+++ client/src/Pages/hotelOwner/Layout.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import Navbar from '../../Components/hotelOwner/Navbar'
 import Sidebar from '../../Components/hotelOwner/Sidebar'
 import { Outlet } from 'react-router-dom'
+import { useAppContext } from '../../context/AppContext.jsx'
+import { useEffect } from 'react'
+
 
 const Layout = () => {
+    const {isOwner , navigate} = useAppContext(); //get the isOwner state from the context to check if the user is a hotel owner
+
+    //if the user is not a hotel owner, redirect to the home page
+    useEffect(()=>{
+        if(!isOwner){
+            navigate('/')
+        }
+    }, [isOwner]) //run this effect when the isOwner state changes or when the navigate function changes;
+
     return (
         <div className='flex flex-col h-screen'>
             <Navbar />
